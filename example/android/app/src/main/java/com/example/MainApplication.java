31dfc204eebda3c8ae38b844ef6b8f0643630572
Stefano Suryanto <stefano@shoguninc.com>
2020-02-15 11:11:42 -0800

    [android] setup example app with api key

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.getbouncer.cardscan.ScanActivity;
 
 import java.util.Arrays;
 import java.util.List;
@@ -43,5 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+
+    ScanActivity.apiKey = "ENTER_API_KEY";
   }
 }
